23545ee8932629801c7ded259f4b61ad91b2d504
seoyun-bae <eiren326@gmail.com>
2026-03-18 03:05:39 +0900

    Add files via upload

---
+++ 0313/66p_3_MidChk.py
@@ -0,0 +1,4 @@
+width = 10
+height = 10
+area = width * height * 1/2
+print("밑변이", width, "이고 높이가", height,"인 삼각형의 면적은", area)
---
+++ 0313/73p_AdvProb.py
@@ -0,0 +1,4 @@
+speed = 60000
+distance = 4E13
+secs = distance/speed
+print("보이저로 가면 걸리는 시간", secs)
